32a678221d982f7a711c91120e50756046a8d0e6
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-02-04 14:14:21 -0500

    update reload.sh

--- reload.sh
+++ reload.sh
@@ -3,5 +3,5 @@
 pnpm run build
 sudo rm -r $HOME/homebrew/plugins/hhd-decky/
 sudo rm -rf $HOME/homebrew/logs/hhd-decky/*
-sudo cp -r $HOME/Development/hhd-decky/ $HOME/homebrew/plugins/
+sudo cp -r ../hhd-decky/ $HOME/homebrew/plugins/
 sudo systemctl restart plugin_loader.service
