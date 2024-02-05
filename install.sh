ca7072e4241a9761e585d17bcd0aa23e6e8ea03f
Tulili <tulilirockz+git@outlook.com>
2024-02-05 10:26:45 -0300

    feat: version-based installer
    Allows users to use the VERSION variable to install different versions of this software like this: `curl $URL | VERSION=0.0.7 sh`

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # does the following:
 # - hhd Decky Plugin
+VERSION=${VERSION:-"LATEST"}
 if [ "$EUID" -eq 0 ]
   then echo "Please do not run as root"
   exit
@@ -14,7 +15,13 @@ cd $HOME
 sudo rm -rf "$HOME/homebrew/plugins/hhd-decky"
 
 echo "installing hhd plugin"
-curl -L $(curl -s "https://api.github.com/repos/hhd-dev/hhd-decky/releases/latest" | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/hhd-decky.tar.gz
+
+FINAL_URL='https://api.github.com/repos/hhd-dev/hhd-decky/releases/latest'
+if [ VERSION != "LATEST" ] ; then
+  FINAL_URL="https://api.github.com/repos/hhd-dev/hhd-decky/releases/tags/v${VERSION}"
+fi
+
+curl -L $(curl -s "${FINAL_URL}" | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/hhd-decky.tar.gz
 sudo tar -xzf hhd-decky.tar.gz -C $HOME/homebrew/plugins
 
 # install complete, remove build dir
