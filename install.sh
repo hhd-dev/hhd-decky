afd36580942e854bc08d70c32301dd39ae1a7d45
antheas <git@antheas.dev>
2024-02-06 18:31:44 +0100

    change extra releases to use p tag and allow installing them using install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # does the following:
 # - hhd Decky Plugin
-VERSION=${PIN_VERSION:-"LATEST"}
+VERSION=${VERSION_TAG:-"LATEST"}
 if [ "$EUID" -eq 0 ]
   then echo "Please do not run as root"
   exit
@@ -18,7 +18,7 @@ echo "installing hhd plugin"
 
 FINAL_URL='https://api.github.com/repos/hhd-dev/hhd-decky/releases/latest'
 if [ $VERSION != "LATEST" ] ; then
-  FINAL_URL="https://api.github.com/repos/hhd-dev/hhd-decky/releases/tags/v${VERSION}"
+  FINAL_URL="https://api.github.com/repos/hhd-dev/hhd-decky/releases/tags/${VERSION}"
 fi
 
 curl -L $(curl -s "${FINAL_URL}" | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/hhd-decky.tar.gz
