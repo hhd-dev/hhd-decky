ae1dffc93d5e8e6672cb1ae518c824475d273e5d
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-02-06 12:56:26 -0500

    add arrow toggle button to ota updates

--- src/components/OtaUpdates.tsx
+++ src/components/OtaUpdates.tsx
@@ -1,20 +1,27 @@
-import { useEffect } from 'react';
-import { getServerApi, otaUpdate } from '../backend/utils';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectDeckyPluginVersionNum, selectLatestDeckyPluginVersionNum } from '../redux-modules/uiSlice';
+import { useEffect } from "react";
+import { getServerApi, otaUpdate } from "../backend/utils";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectDeckyPluginVersionNum,
+  selectLatestDeckyPluginVersionNum,
+} from "../redux-modules/uiSlice";
 import {
   ButtonItem,
   Field,
   PanelSection,
-  PanelSectionRow
-} from 'decky-frontend-lib';
-import { AppDispatch } from '../redux-modules/store';
-import { fetchDeckyPluginVersion, fetchLatestPluginVersion } from '../redux-modules/hhdAsyncThunks';
+  PanelSectionRow,
+} from "decky-frontend-lib";
+import { AppDispatch } from "../redux-modules/store";
+import {
+  fetchDeckyPluginVersion,
+  fetchLatestPluginVersion,
+} from "../redux-modules/hhdAsyncThunks";
+import ArrowToggleButton from "./ArrowToggleButton";
 
 const OtaUpdates = () => {
   const installedVersionNum = useSelector(selectDeckyPluginVersionNum);
   const latestVersionNum = useSelector(selectLatestDeckyPluginVersionNum);
-  const dispatch = useDispatch<AppDispatch>()
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(fetchDeckyPluginVersion());
@@ -28,37 +35,39 @@ const OtaUpdates = () => {
   }
 
   return (
-    <PanelSection title="Updates">
-      <PanelSectionRow>
-        <Field disabled label={'Installed Version'}>
-          {installedVersionNum}
-        </Field>
-      </PanelSectionRow>
-
-      {Boolean(latestVersionNum) && (
+    <PanelSection title="Decky Version">
+      <ArrowToggleButton cacheKey="hhd.arrowToggleButton.otaUpdates">
         <PanelSectionRow>
-          <Field disabled label={'Latest Version'}>
-            {latestVersionNum}
+          <Field disabled label={"Installed Version"}>
+            {installedVersionNum}
           </Field>
         </PanelSectionRow>
-      )}
-      <PanelSectionRow>
-        <ButtonItem
-          onClick={() => {
-            const serverApi = getServerApi();
-            if (serverApi) otaUpdate(serverApi);
-          }}
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-          layout={'below'}
-        >
-          {buttonText}
-        </ButtonItem>
-      </PanelSectionRow>
+
+        {Boolean(latestVersionNum) && (
+          <PanelSectionRow>
+            <Field disabled label={"Latest Version"}>
+              {latestVersionNum}
+            </Field>
+          </PanelSectionRow>
+        )}
+        <PanelSectionRow>
+          <ButtonItem
+            onClick={() => {
+              const serverApi = getServerApi();
+              if (serverApi) otaUpdate(serverApi);
+            }}
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            layout={"below"}
+          >
+            {buttonText}
+          </ButtonItem>
+        </PanelSectionRow>
+      </ArrowToggleButton>
     </PanelSection>
   );
 };
